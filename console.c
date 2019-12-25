24c274e3c33dbe0bd481fc0755767481162eaac1
xiaowei9798 <xiaowei7176@163.com>
2019-12-25 21:46:43 +0800

    为应用程序自由命名，注意NULL的ASCLL为0，在保存文件名时，对结尾应用了ASCLL的判断

--- console.c
+++ console.c
@@ -181,13 +181,13 @@ void cons_runcmd(char *cmdline,struct CONSOLE *cons,int *fat,unsigned int memtot
         cmd_dir(cons);
     }else if(strncmp(cmdline,"TYPE ",5)==0){
         cmd_type(cons,fat,cmdline);
-    }else if(strcmp(cmdline,"HELLO")==0){
-        cmd_hlt(cons,fat);
     }else if(cmdline[0]!=0){
-        putfonts8_asc_sht(cons->sht,8,cons->cur_y,COL8_FFFFFF,COL8_000000,"Bad command.",12);
-        cons_newline(cons);
-        cons_newline(cons);
-    }
+		if(cmd_app(cons,fat,cmdline)==0){
+			putfonts8_asc_sht(cons->sht,8,cons->cur_y,COL8_FFFFFF,COL8_000000,"Bad command.",12);
+			cons_newline(cons);
+			cons_newline(cons);
+		}
+	}
     return;
 }
 
@@ -269,26 +269,40 @@ void cmd_type(struct CONSOLE *cons,int *fat,char *cmdline)
 	return;
 }
 
-void cmd_hlt(struct CONSOLE *cons,int *fat)
+int cmd_app(struct CONSOLE *cons, int *fat,char *cmdline)
 {
-    struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	struct FILEINFO *finfo = file_search("HELLO.HRB", (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
-	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
-	char *p;
-	if (finfo != 0) {
-		/* ファイルが見つかった場合 */
-		p = (char *) memman_alloc_4k(memman, finfo->size);
-		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
-		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);
-		farcall(0, 1003 * 8);
-		memman_free_4k(memman, (int) p, finfo->size);
-	} else {
-		/* ファイルが見つからなかった場合 */
-		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
-		cons_newline(cons);
-	}
-	cons_newline(cons);
-	return;
-}
+	struct MEMMAN *memman=(struct MEMMAN *)MEMMAN_ADDR;
+	struct FILEINFO *finfo;
+	struct SEGMENT_DESCRIPTOR *gdt=(struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+	char name[18],*p;
+	int i;
 
+	for(i=0;i<13;i++){
+		if(cmdline[i] <=' '){ //空格的ASCLL为32，NULL为0，32一下的都是控制字符
+			break;
+		}
+		name[i]=cmdline[i];
+	}
+	name[i]=0;
+
+	finfo=file_search(name ,(struct FILEINFO *)(ADR_DISKIMG+0x002600),224);
+	if(finfo==0&&name[i-1]!='.'){
+		name[i]='.';
+		name[i+1]='H';
+		name[i+2]='R';
+		name[i+3]='B';
+		name[i+4]=0;
+		finfo=file_search(name,(struct FILEINFO *)(ADR_DISKIMG+0x002600),224);
+	}	
+	if(finfo!=0){
+		p=(char *)memman_alloc_4k(memman,finfo->size);
+		file_loadfile(finfo->clustno,finfo->size,p,fat,(char *)(ADR_DISKIMG+0x003e00));
+		set_segmdesc(gdt+1003,finfo->size-1,(int)p ,AR_CODE32_ER);
+		farcall(0,1003*8);
+		memman_free_4k(memman,(int) p,finfo->size);
+		cons_newline(cons);
+		return 1;
+	}
+	return 0;
+}
 
