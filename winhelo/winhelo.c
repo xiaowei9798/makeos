9eda0d0b5c52185251459df18348819d4ec26900
xiaowei9798 <xiaowei7176@163.com>
2020-01-13 16:44:13 +0800

    对之前函数外部声明变量的方法进行改写，注意对比两种方法makefile文件中SK和ML空间大小的定义

--- winhelo/winhelo.c
+++ winhelo/winhelo.c
@@ -1,10 +1,12 @@
 #include "apilib.h"
 
-char buf[150 * 50];
+// char buf[150 * 50];  之前采用的方法，在函数外部声明的变量，会被解释成DB和RESB；函数内部声明的非static变量会从栈中分配空间
 
 void HariMain(void)
 {
     int win;
+    char buf[150*50];  //当增加了__alloca函数后，采用内部声明变量的方法，此时注意修改makefile函数中栈空间大小的分配
+
     win = api_openwin(buf, 150, 50, -1, "hello");
     for(;;)
     {
--- winhelo3/winhelo3.c
+++ winhelo3/winhelo3.c
@@ -6,7 +6,8 @@ void HariMain(void)
     int win;
 
     api_initmalloc();
-    buf=api_malloc(150*50);
+    buf=api_malloc(150*50);  //winhelo3采用的是malloc方法，其与_alloca方法生成的.hrb文件的字节大小差距不大
+
     win = api_openwin(buf, 150, 50, -1, "hello");
     api_boxfilwin(win, 8, 36, 141, 43, 6);
     api_putstrwin(win, 28, 28, 0, 12, "hello, world");
